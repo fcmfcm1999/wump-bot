ddcf6a15643b35f742972308e75274d7715b2036
0xFantasy <fcmfantasy1999@gmail.com>
2025-06-04 23:21:37 +0800

    feat: add proxy

--- wump.js
+++ wump.js
@@ -1,6 +1,10 @@
 const axios = require('axios')
 const fs = require('fs').promises
-const userAgents = require('user-agents')
+const path = require('path')
+const { readFileSync } = require('node:fs')
+const { SocksProxyAgent } = require('socks-proxy-agent')
+
+const ADDRESSES_FILE_PATH = path.join(__dirname, 'tokens.json')
 
 const colors = {
     green: '\x1b[32m',
@@ -66,7 +70,13 @@ const tasksHeaders = (token, userAgent) => ({
     apikey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTczNzQ2NjYyMCwiZXhwIjo0ODkzMTQwMjIwLCJyb2xlIjoiYW5vbiJ9.qSJu05pftBJrcqaHfX5HZC_kp_ubEWAd0OmHEkNEpIo',
 })
 
-async function getUserInfo(token, userAgent) {
+async function getUserInfo(account, userAgent) {
+    const { token, proxy } = account
+    let httpsAgent
+    if (proxy != null && proxy !== '') {
+        httpsAgent = new SocksProxyAgent(`${proxy}`)
+    }
+
     try {
         const authResponse = await axios.put(
             'https://api.wump.xyz/auth/v1/user',
@@ -82,6 +92,7 @@ async function getUserInfo(token, userAgent) {
                     'x-client-info': 'supabase-ssr/0.5.2',
                     'x-supabase-api-version': '2024-01-01',
                 },
+                httpsAgent,
             }
         )
         const userId = authResponse.data.id
@@ -89,6 +100,7 @@ async function getUserInfo(token, userAgent) {
             `https://api.wump.xyz/rest/v1/users?select=*&id=eq.${userId}`,
             {
                 headers: userHeaders(token, userAgent),
+                httpsAgent,
             }
         )
         return userResponse.data
@@ -98,10 +110,17 @@ async function getUserInfo(token, userAgent) {
     }
 }
 
-async function getTasks(token, userAgent) {
+async function getTasks(account, userAgent) {
+    const { token, proxy } = account
+    let httpsAgent
+    if (proxy != null && proxy !== '') {
+        httpsAgent = new SocksProxyAgent(`${proxy}`)
+    }
+
     try {
         const response = await axios.get('https://api.wump.xyz/rest/v1/tasks?select=*', {
             headers: tasksHeaders(token, userAgent),
+            httpsAgent,
         })
         return response.data
     } catch (error) {
@@ -110,12 +129,19 @@ async function getTasks(token, userAgent) {
     }
 }
 
-async function getUserTasks(token, userId, userAgent) {
+async function getUserTasks(account, userId, userAgent) {
+    const { token, proxy } = account
+    let httpsAgent
+    if (proxy != null && proxy !== '') {
+        httpsAgent = new SocksProxyAgent(`${proxy}`)
+    }
+
     try {
         const response = await axios.get(
             `https://api.wump.xyz/rest/v1/user_tasks?select=*&user_id=eq.${userId}`,
             {
                 headers: tasksHeaders(token, userAgent),
+                httpsAgent,
             }
         )
         return response.data
@@ -125,7 +151,13 @@ async function getUserTasks(token, userId, userAgent) {
     }
 }
 
-async function completeSocialTask(token, taskId, userAgent) {
+async function completeSocialTask(account, taskId, userAgent) {
+    const { token, proxy } = account
+    let httpsAgent
+    if (proxy != null && proxy !== '') {
+        httpsAgent = new SocksProxyAgent(`${proxy}`)
+    }
+
     try {
         const response = await axios.post(
             'https://api.wump.xyz/functions/v1/api/tasks/social_follow',
@@ -134,6 +166,7 @@ async function completeSocialTask(token, taskId, userAgent) {
             },
             {
                 headers: getCommonHeaders(token, userAgent),
+                httpsAgent,
             }
         )
         return response.data
@@ -143,91 +176,85 @@ async function completeSocialTask(token, taskId, userAgent) {
     }
 }
 
-async function completeReferralTask(token, taskId, userAgent) {
-    try {
-        const response = await axios.post(
-            'https://api.wump.xyz/functions/v1/api/tasks/referral',
-            {
-                taskid: taskId,
-            },
-            {
-                headers: getCommonHeaders(token, userAgent),
-            }
-        )
-        return response.data
-    } catch (error) {
-        logger.error(`Failed to complete referral task ${taskId}: ${error.message}`)
-        return null
-    }
+function parseToken(token) {
+    const jsonStr = Buffer.from(token, 'base64').toString('utf8')
+    const endIndex = jsonStr.indexOf('\"user\"')
+    const jsonObj = JSON.parse(jsonStr.slice(0, endIndex - 1) + '}')
+
+    return jsonObj
 }
 
 async function main() {
     logger.banner()
 
     try {
-        const tokensData = await fs.readFile('tokens.txt', 'utf8')
-        const tokens = tokensData
-            .split('\n')
-            .map((line) => line.trim())
-            .filter((line) => line)
+        const data = readFileSync(ADDRESSES_FILE_PATH, 'utf-8')
+        const accounts = JSON.parse(data)
 
-        for (const token of tokens) {
-            logger.step(`Processing token: ${token.slice(0, 20)}...`)
-
-            // const userAgent = new userAgents().toString()
-            const userAgent =
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0'
-            logger.agent(`Using User-Agent: ${userAgent}`)
-
-            logger.loading('Fetching user information...')
-            const userInfo = await getUserInfo(token, userAgent)
-            if (userInfo) {
-                logger.info(`User: ${userInfo.username}`)
-                logger.info(`Global Name: ${userInfo.global_name}`)
-                logger.wallet(`Total Points: ${userInfo.total_points}`)
-            } else {
-                logger.error('Skipping token due to user info fetch failure')
-                continue
-            }
-
-            logger.loading('Fetching tasks...')
-            const tasks = await getTasks(token, userAgent)
-            const completedTasks = await getUserTasks(token, userInfo.id, userAgent)
-            const completedTaskIds = completedTasks.map((it) => it.task_id)
-
-            const uncompletedTasks = tasks.filter(
-                (it) => it.task_type != 'referral' && !completedTaskIds.includes(it.id)
-            )
-            if (uncompletedTasks.length === 0) {
-                logger.info("current no tasks")
-            }
-
-            for (const task of uncompletedTasks) {
-                logger.step(`Processing task: ${task.task_description} (${task.points} points)`)
-
-                let result
-                if (task.task_type === 'referral') {
-                    continue
-                } else {
-                    result = await completeSocialTask(token, task.id, userAgent)
-                }
-
-                if (result && result.success) {
-                    logger.success(
-                        `Completed task: ${task.task_description} - Earned ${result.result.earned} points`
-                    )
-                    logger.info(`Total Points: ${result.result.total_points}`)
-                } else {
-                    logger.error(`Failed to complete task: ${task.task_description}`)
-                }
-
-                await new Promise((resolve) => setTimeout(resolve, 2000))
-            }
-
-            logger.success('Finished processing all tasks for this token\n')
+        const tokenInfo = parseToken(accounts[0].token)
+        if (Date.now() / 1000 > tokenInfo.expires_at) {
+            console.log('expired!')
         }
 
-        logger.success('All tokens processed successfully!')
+        // for (const account of accounts) {
+        const account = accounts[0]
+        account.token = tokenInfo.access_token
+        logger.step(`Processing account: ${account.id}`)
+
+        const userAgent =
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0'
+        logger.agent(`Using User-Agent: ${userAgent}`)
+
+        logger.loading('Fetching user information...')
+        const userInfo = await getUserInfo(account, userAgent)
+        if (userInfo) {
+            logger.info(`User: ${userInfo.username}`)
+            logger.info(`Global Name: ${userInfo.global_name}`)
+            logger.wallet(`Total Points: ${userInfo.total_points}`)
+        } else {
+            logger.error('Skipping account due to user info fetch failure')
+            // continue
+        }
+
+        logger.loading('Fetching tasks...')
+        const tasks = await getTasks(account, userAgent)
+        const completedTasks = await getUserTasks(account, userInfo.id, userAgent)
+        const completedTaskIds = completedTasks.map((it) => it.task_id)
+
+        const uncompletedTasks = tasks.filter(
+            (it) => it.task_type != 'referral' && !completedTaskIds.includes(it.id)
+        )
+        if (uncompletedTasks.length === 0) {
+            logger.info('current no tasks')
+            uncompletedTasks.push(tasks[0])
+        }
+
+        for (const task of uncompletedTasks) {
+            logger.step(`Processing task: ${task.task_description} (${task.points} points)`)
+
+            let result
+            if (task.task_type === 'referral') {
+                continue
+            } else {
+                result = await completeSocialTask(account, task.id, userAgent)
+            }
+
+            if (result && result.success) {
+                logger.success(
+                    `Completed task: ${task.task_description} - Earned ${result.result.earned} points`
+                )
+                logger.info(`Total Points: ${result.result.total_points}`)
+            } else {
+                logger.error(`Failed to complete task: ${task.task_description}`)
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+        }
+
+        logger.success('Finished processing all tasks for this account\n')
+        // }
+
+        logger.success('All account processed successfully!')
     } catch (error) {
         logger.error(`Error: ${error.message}`)
     }
